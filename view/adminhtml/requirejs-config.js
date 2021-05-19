197d84deec9914144c25b4b4e3eb2ea5fdb472f9
Carlos Cerda <53513374+ccerda0520@users.noreply.github.com>
2021-05-18 23:40:08 -0700

    Update requirejs-config.js

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     config: {
         mixins: {
             "Magento_PageBuilder/js/converter/style/min-height": {
-                "ComponentBasedCarlos_PagebuilderEnhancements/js/converter/style/min-height-mixin": true
+                "ComponentBasedCarlos_PageBuilderEnhancements/js/converter/style/min-height-mixin": true
             }
         }
     }
